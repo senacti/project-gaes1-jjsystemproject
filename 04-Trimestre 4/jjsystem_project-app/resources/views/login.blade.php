dbdb1a2643093791db84ffd3ff906f5c58f7ac3e
Mafe <ferdlgpar@gmail.com>
2023-06-10 21:32:26 -0500

    .......

--- 04-Trimestre 4/jjsystem_project-app/resources/views/login.blade.php
+++ 04-Trimestre 4/jjsystem_project-app/resources/views/login.blade.php
@@ -9,19 +9,22 @@
 </head>
 
 <body>
-<Section class="contenedorPrincipal ">
+<Section class="contenedorPrincipal">
+    <span class="icon-close">
+      <ion-icon name="close-outline"></ion-icon>
+    </span>
     <div class = "contenedor-login" >
-        <h1 class="Titulo">Iniciar Sesion</h1>
-            <form action="">
+      <h1 class="Titulo">Iniciar Sesion</h1>
+          <form action="">
             <div class="icon-contenedor">
-                <img  src="../../images/FormulariosIMG/login.png" class="icon">
+              <img  src="../../images/FormulariosIMG/login.png" class="icon">
             </div>  
-                <div class="Datos">
-                <div class="Input-datos-login">
-                    <label>Numero de Documento</label>
-                    <input type="number" class="input" required>
-                    <label>Contraseña</label>
-                    <input type="password" class="input" required>
+              <div class="Datos">
+                <div class="Input-datos-formularios">
+                  <label>Email</label>
+                  <input type="email" class="input" required  id="email">
+                  <label>Contraseña</label>
+                  <input type="password" class="input" required id="password">
                 </div>
                 <div class="botonC">
                     <input type="submit" value="Iniciar Sesion" id="iniciarSesion">
@@ -29,11 +32,13 @@
                 <div class="recuperar-contraseña">
                     <a id="recuperarContraseña" href="../formularios/recuperarcontraseña.html">Olvide mi contraseña</a></li>
                 </div>
-            </div>
+                <div class="login-registrar">
+                  <a id="registrar" href="#">Registrarse</a></li>
+              </div>
+              </div>
         </form>
-    </div>
-    
-</section>
+      </div>
+  </Section>
     
 </body>
 </html>
--- 04-Trimestre 4/jjsystem_project-app/resources/views/registro.blade.php
+++ 04-Trimestre 4/jjsystem_project-app/resources/views/registro.blade.php
@@ -6,88 +6,65 @@
     <title>Registrarse</title>
 </head>
 <body>
-    <div class="contenedorPrincipal">
-        <h1 class="Titulo">Registrarse</h1>
-        <form action="">
-            <div class="Datos">
-                <div class="Input-datos">
-                    <label for="Nombre">Nombre</label>
-                    <input type="text"
-                            id="Nombre"
-                            name="Nombre"
-                            required/>
-                </div>
-                <div class="Input-datos">
-                    <label for="Apellido">Apellido</label>
-                    <input type="text"
-                            id="Apellido"
-                            name="Apellido"
-                            required/>
-                </div>
-                <div class="Input-datos-bloque">
-                    <label for="Correo">Correo electronico</label>
-                    <input type="email"
-                            id="Correo"
-                            name="Correo"
-                            required/>
-                </div>
-                
-                <div class="Input-datos">
-                    <label for="NumeroTelefono">Numero de teléfono</label>
-                    <input type="number"
-                            id="NumeroTelefono"
-                            name="NumeroTelefono" required max=""/>
-                </div>
-                <div class="Input-datos">
-                    <label for="pais">Pais</label>
-                    <select name=""  required>
-                        <option value="Colombia"></option>
-                        <option value="Colombia">Colombia</option>
-                        <option value="Mexico">Mexico</option>
-                    </select>    
-                    <datalist>
-                    </div>
-                
-                <div class="Input-datos">
-                    <label for="NumeroDocumento">Numero de documento</label>
-                    <input type="number"
-                            id="NumeroDocumento"
-                            name="NumeroDocumento"
-                            required/>
-                </div>
-                <div class="Input-datos">
-                    <label for="TipoDoc">Tipo Documento</label>
-                    <select name=""  required>
-                        <option value="Cedula de ciudadania">
-                            Cedula de ciudadania</option>
-                        <option value="Pasaporte">Pasaporte</option>
-                        <option value="NIT">NIT</option>
-                    </select>    
-                    <datalist>
-                </div>
-                <div class="Input-datos">
-                    <label for="Contraseña">Contraseña</label>
-                    <input type="password"
-                            id="Contraseña"
-                            name="Contraseña"
-                            required/>
-                </div>
-                <div class="Input-datos">
-                    <label for="ConfirmarContraseña">Confirmar contraseña</label>
-                    <input type="password"
-                            id="ConfirmarContraseña"
-                            name="ConfirmarContraseña"
-                            required/>
-                </div>
+<Section class="contenedorPrincipal">
+        <span class="icon-close">
+          <ion-icon name="close-outline"></ion-icon>
+        </span>
+        <div class = "contenedor-register" >
+            <h1 class="Titulo">Registrarse</h1>
+                <form action="">
+                  <!--Parte uno-->
+                    <div class="parte1">
+                      <div class="Datos">
+                        <div class="Input-datos-formularios">
+                            <label>Nombre</label>
+                            <input type="text" id="Nombre" name="Nombre" required/>
+                            <label >Apellido</label>
+                            <input type="text" id="Apellido" name="Apellido" required/>
+                            <label>Numero de teléfono</label>
+                            <input type="number" id="NumeroTelefono" name="NumeroTelefono" required max=""/>
+                            <label>Email</label>
+                            <input type="email" class="input" name="email*">
+                            <label >Tipo Documento</label>
+                            <select name="TipoDoc" required>
+                                <option value="selecciona" class="selecciona">Selecciona</option>
+                                <option value="Cedula de ciudadania"class="selecciona">Cedula de ciudadania</option>
+                                <option value="Pasaporte"class="selecciona">Pasaporte</option>
+                                <option value="NIT"class="selecciona">NIT</option>
+                            </select>
+                            <label for="NumeroDocumento">Numero de documento</label>
+                            <input type="number" id="NumeroDocumento"name="NumeroDocumento" required/>
+                            <div class="botonC" id="Continuar">
+                              <input type="submit" value="Continuar" >
+                            </div>
+                            <div class="login-registrar">
+                              <a id="login" href="#">Iniciar Sesion</a></li>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!--Parte dos-->
+                      <div class="Parte2" id="parte-2">
+                            <div class="Datos">
+                              <div class="Input-datos-formularios">
+                                <label>Contraseña</label>
+                                <input type="password" class="input" required id="password1">
+                                <label >Confirmar contraseña</label>
+                                <input type="password"id="ConfirmarContraseña" name="ConfirmarContraseña"required/>
+                          
+                              <div class="check-botton">
+                                <input type="radio" name="AceptaTerminos" id="AceptaTerminos" required/>
+                                <label >Acepto los terminos y condiciones. </label>
+                              </div>  
+                                <div class="botonC">  
+                                  <input type="submit" value="Registrarse" id="link-registrarse">
+                                </div >
+                              </div>
+                          </div>
+                      </div>    
+                </form>
             </div>
-            <div class="botones">
-                <input type="radio" name="AceptaTerminos" id="AceptaTerminos" required/>
-                <label for="AceptaTerminos">He leido y acepto los terminos de las politicas de seguridad y tratamiento de datos</label>
-                <div class="botonC" id="crear-cuenta-boton">
-                <input type="submit" value="CREAR CUENTA">
-            </div>
-        </div>
-        </form>
-    </div>
-</body>
+      </section>
+      <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script src="../../JavaScript/funcionesFormularios.js"></script>
 </html>
