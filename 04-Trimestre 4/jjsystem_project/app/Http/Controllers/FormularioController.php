f9d69b0b7d7ece352696abaafd340399b10d270b
AngieEspejo <115506879+AngieEspejo@users.noreply.github.com>
2023-06-22 07:14:42 -0500

    .

---
+++ 04-Trimestre 4/jjsystem_project/app/Http/Controllers/FormularioController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Formulario;
+use Illuminate\Http\Request;
+
+class FormularioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $Formulario = Formulario::all();
+        return view('formulario/visualizar',compact('Formulario'));
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Formulario $formulario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Formulario $formulario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Formulario $formulario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Formulario $formulario)
+    {
+        //
+    }
+}
--- 04-Trimestre 4/jjsystem_project/routes/web.php
+++ 04-Trimestre 4/jjsystem_project/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginRegisterController;
+use App\Http\Controllers\Auth\FormularioPqrsfController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
