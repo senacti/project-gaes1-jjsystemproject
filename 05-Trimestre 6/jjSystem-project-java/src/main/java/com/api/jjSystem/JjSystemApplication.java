4f0c989c7b400c864cb004e8a03b7cdcb2316305
Mafe <ferdlgpar@gmail.com>
2023-10-10 21:04:54 -0500

    creacion de carpeta  trimestre 6 y proyecto en java

---
+++ 05-Trimestre 6/jjSystem-project-java/src/main/java/com/api/jjSystem/JjSystemApplication.java
@@ -0,0 +1,13 @@
+package com.api.jjSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjSystemApplication.class, args);
+	}
+
+}
---
+++ 05-Trimestre 6/jjSystem-project-java/src/test/java/com/api/jjSystem/JjSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jjSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
